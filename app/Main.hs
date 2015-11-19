98ea12770de04e5299690fb189e8194e09743526
Irio Musskopf <iirineu@gmail.com>
2015-11-19 17:12:09 -0500

    Run example from the lib

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,19 @@
 module Main where
 
-import Lib
+import Network.Wreq ( defaults )
+import Data.Text.Encoding ( encodeUtf8 )
+import Data.Text ( pack )
+import Control.Lens ( (^.) )
+import PivotalTrackerApi ( withToken
+                         , getStory
+                         )
+import PivotalTrackerApi.Story ( name )
 
 main :: IO ()
-main = someFunc
+main = do
+    story <- getStory authentication projectId storyId
+    print $ story ^. name
+    where
+      authentication = withToken "7f3f76bc6ae8c48e7b528369c999c8d8" defaults
+      projectId = 1440520
+      storyId = 104591424
